47c244292bdfd0c013d53c018879da730b5956b9
Henry Dormon <henry.dormon@gmail.com>
2024-02-28 14:51:49 +0000

    removed console log

--- app/components/AddFriendButton.tsx
+++ app/components/AddFriendButton.tsx
@@ -34,7 +34,6 @@ export const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
 
       setShowSuccessState(true);
     } catch (error) {
-      console.log(error);
       if (error instanceof z.ZodError) {
         setError("email", { message: error.message });
         return;
@@ -61,7 +60,7 @@ export const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
       </label>
       <div className="mt-2 flex gap-4">
         <input
-          {...register("email")}
+          {...register("email")} // needed for accessiblity: onchange, blur, ref
           type="text"
           className="block w-full rounded-md border-0 py-1.5 text-gray-900m shadow-sm ring-1 ring-inset ring-grey-300 placeholder:text-grey-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           placeholder="you@example.com"
